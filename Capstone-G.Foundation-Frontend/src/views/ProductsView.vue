9c78425c61a41663c41c4d72a6660d62cce87087
SIPHAMANDLAMLANJANA <114500540+Siphamandla201@users.noreply.github.com>
2023-03-14 11:50:58 +0200

    1-push

---
+++ Capstone-G.Foundation-Frontend/src/views/ProductsView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="Navbar">
+    <NavbarComp />
+  </div>
+  <body>
+    <div class="filter">
+      <FilterComp style="width: 100%" />
+    </div>
+    <div class="Products">
+      <ProductsCard />
+    </div>
+  </body>
+  <footer>
+    <Footer></Footer>
+  </footer>
+</template>
+<script>
+import NavbarComp from "../components/NavbarComp.vue";
+import ProductsCard from "@/components/ProductsCard.vue";
+import FilterComp from "@/components/FilterComp.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Products",
+  components: {
+    NavbarComp,
+    ProductsCard,
+    FilterComp,
+    Footer,
+  },
+};
+</script>
+<style scoped>
+body {
+  display: flex;
+}
+
+.Navbar {
+  height: 500px;
+  background-image: url("https://i.postimg.cc/4xm62szp/Moncler.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.filter {
+  width: 25%;
+}
+
+.Products {
+  width: 75%;
+}
+</style>
